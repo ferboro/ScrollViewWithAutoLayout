b305f043e64cce8110a51861474b16ee435fadd3
wwwins <jacky.huang@isobar.com>
2014-12-09 16:59:00 +0800

    Init import.

---
+++ ScrollViewWithAutoLayout/ScrollViewWithAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewWithAutoLayout
+//
+//  Created by wwwins on 2014/12/9.
+//  Copyright (c) 2014年 isobar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
